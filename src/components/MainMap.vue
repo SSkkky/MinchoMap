2e5b19b0993a9c60f08ae64794aee4ed31c1a7e6
SSkkky <worte5633@gmail.com>
2024-04-05 23:55:19 +0900

    카카오 지도 불러오기 성공

--- src/components/MainMap.vue
+++ src/components/MainMap.vue
@@ -2,13 +2,12 @@
 import { onMounted } from 'vue';
 
 const initMap = () => {
-  const mapContainer = document.getElementById('map'); // 지도를 표시할 div
+  const mapContainer = document.getElementById('map');
   const mapOption = {
-    center: new kakao.maps.LatLng(37.56620, 126.92053), // 지도의 중심좌표
-    level: 1, // 지도의 확대 레벨
+    center: new kakao.maps.LatLng(37.56620, 126.92053),
+    level: 1,
   };
-    const map = new kakao.maps.Map(mapContainer, mapOption);
-  //https://apis.map.kakao.com/web/sample/changeLevel/
+  new kakao.maps.Map(mapContainer as HTMLElement, mapOption);
 };
 
 onMounted(() => {
@@ -16,21 +15,23 @@ onMounted(() => {
     initMap();
   } else {
     const script = document.createElement('script');
-    script.onload = () => kakao.maps.load(initMap);
     script.src = 'http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=a7c8dc5e9a80d7a0d9c12c5d44404383';
     document.head.appendChild(script);
+    script.onload = () => kakao.maps.load(initMap);
   }
 });
 </script>
 
 
 <template>
-  <div id="map" style="width:500px;height:400px;"></div>
+  <div id="map"></div>
 </template>
 
 <style scoped>
 #map{
-    width:500px;
-    height:400px;
+    width:100vw;
+    height:100vh;
+    margin: 0;
+    padding: 0;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import './style.css'
+import './assets/styles/reset.css'
 import App from './views/App.vue'
 
 createApp(App).mount('#app')
