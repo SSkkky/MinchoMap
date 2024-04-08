100bd09a20d9d2dcb6d1d291b2644d4482d0fb4b
SSkkky <worte5633@gmail.com>
2024-04-08 18:21:29 +0900

    detail 수정중

--- src/components/DetailComp.vue
+++ src/components/DetailComp.vue
@@ -1,6 +1,23 @@
 <script setup lang="ts">
 import { useRoute } from 'vue-router'
+import { mapDataType } from '../types/DataType';
+import { useStore } from 'vuex';
+import { onMounted, computed, ComputedRef } from 'vue';
 
+const store = useStore();
+const data: ComputedRef<mapDataType[]> = computed(() => (store.state.data));
+
+async function fetchData() {
+    await store.getters.getData;
+    const filteredData = data.value.filter((item) => item.id === Number(route.params));
+    console.log(filteredData)
+}
+
+onMounted(() => {
+    fetchData();
+});
+
+// data 에서 id값이 일치하는 객체를 찾아 반환하기 ㅇㅅㅇ
 
 const route = useRoute()
 console.log(route.params)
