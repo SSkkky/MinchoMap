4294020090e96a2071b29b8b63364538c33c74ea
SSkkky <worte5633@gmail.com>
2024-04-11 10:32:01 +0900

    revert

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import HomeView from '../views/Home.vue'
 import DetailView from '../views/Detail.vue'
 
 const router = createRouter({
-    history: createWebHistory(`${import.meta.env.VITE_BASE_URL}`),
+    history: createWebHistory(),
     routes: [
         {
             path: '/',
